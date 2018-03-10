f7559d965bd88f62959e428a54036560344de053
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-10 10:26:15 +0200

    Replaced Toggl library with personal

--- src/main/kotlin/io/github/dmitrychaban/toggl2jira/Toggl2jiraApplication.kt
+++ src/main/kotlin/io/github/dmitrychaban/toggl2jira/Toggl2jiraApplication.kt
@@ -1,10 +1,19 @@
 package io.github.dmitrychaban.toggl2jira
 
+import io.github.dmitrychaban.java_toggl_sdk_light.TogglApiBuilder
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Toggl2jiraApplication
+class Toggl2jiraApplication : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        val api = TogglApiBuilder.with(null).build()
+        println(api.context.block()!!.fullname)
+
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Toggl2jiraApplication>(*args)
