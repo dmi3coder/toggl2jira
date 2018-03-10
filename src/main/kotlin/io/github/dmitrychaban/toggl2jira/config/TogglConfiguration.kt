a83973b3dd70471b4c9091495c83d7b775928db6
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-10 10:29:08 +0200

    Removed third-party TogglApi from TogglConfiguration

--- src/main/kotlin/io/github/dmitrychaban/toggl2jira/config/TogglConfiguration.kt
+++ src/main/kotlin/io/github/dmitrychaban/toggl2jira/config/TogglConfiguration.kt
@@ -1,7 +1,5 @@
 package io.github.dmitrychaban.toggl2jira.config
 
-import io.rocketbase.toggl.api.TogglReportApi
-import io.rocketbase.toggl.api.TogglReportApiBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,13 +7,4 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class TogglConfiguration {
 
-    @Value("\${TOGGL_TOKEN}")
-    private lateinit var togglToken: String
-
-    @Bean
-    fun getTogglReportApi(): TogglReportApi {
-        return TogglReportApiBuilder()
-                .setApiToken(togglToken)
-                .setWorkspaceId(2510926).build()
-    }
 }
