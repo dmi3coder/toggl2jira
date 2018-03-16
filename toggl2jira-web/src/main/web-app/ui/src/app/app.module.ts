42ea2d0053c59e4769caba2dfeb0b8e886bd0b7a
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-16 14:54:56 +0200

    Routing raw

--- toggl2jira-web/src/main/web-app/ui/src/app/app.module.ts
+++ toggl2jira-web/src/main/web-app/ui/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { SettingsComponent } from './settings/settings.component';
+import { ConverterComponent } from './converter/converter.component';
 
+
+const appRoutes: Routes = [
+  {path: 'converter', component: ConverterComponent},
+  {path: 'settings', component:  SettingsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SettingsComponent
+    SettingsComponent,
+    ConverterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
