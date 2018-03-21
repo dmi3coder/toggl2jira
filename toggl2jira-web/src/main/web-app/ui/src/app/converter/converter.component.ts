3a5c2531603dec21ba8e4b17000984450b62aa7d
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-21 15:21:54 +0200

    Toggl-timer fetching usage

--- toggl2jira-web/src/main/web-app/ui/src/app/converter/converter.component.ts
+++ toggl2jira-web/src/main/web-app/ui/src/app/converter/converter.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {TogglTimer} from "../toggl-timer";
+import {element} from "protractor";
+import {TogglService} from "../toggl.service";
+import {Inject} from "@angular/compiler/src/core";
 
 @Component({
   selector: 'app-converter',
   templateUrl: './converter.component.html',
   styleUrls: ['./converter.component.css']
 })
+@Injectable()
 export class ConverterComponent implements OnInit {
+  elements: [TogglTimer];
 
-  constructor() { }
+  constructor(private service: TogglService) {
+    service.getTogglTimers().subscribe(items => {
+      this.elements = items;
+      console.log("working");
+      console.log(items)
+    });
+  }
 
   ngOnInit() {
   }
