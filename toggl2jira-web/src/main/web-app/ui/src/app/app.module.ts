ae5e4966d3ba8007b174fdb5e26c01562403370e
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-21 15:21:32 +0200

    Added toggl service Toggl-timer fetching raw

--- toggl2jira-web/src/main/web-app/ui/src/app/app.module.ts
+++ toggl2jira-web/src/main/web-app/ui/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AsyncLocalStorageModule } from 'angular-async-local-storage';
 import { AppComponent } from './app.component';
 import { SettingsComponent } from './settings/settings.component';
 import { ConverterComponent } from './converter/converter.component';
+import { TogglTaskComponent } from './converter/toggl/toggl-task/toggl-task.component';
+import {TogglService} from "./toggl.service";
+import {HttpClientModule} from "@angular/common/http";
 
 
 const appRoutes: Routes = [
@@ -18,17 +21,22 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     SettingsComponent,
-    ConverterComponent
+    ConverterComponent,
+    TogglTaskComponent
   ],
   imports: [
     BrowserModule,
     AsyncLocalStorageModule,
+    HttpClientModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
-  providers: [],
+  providers: [
+    HttpClientModule,
+    TogglService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ toggl2jira-web/src/main/web-app/ui/src/app/toggl.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from "rxjs/Observable";
+import {TogglTimer} from "./toggl-timer";
+import {AsyncLocalStorage} from "angular-async-local-storage";
+import {SettingsComponent} from "./settings/settings.component";
+
+@Injectable()
+export class TogglService {
+  private togglUrl = 'http://localhost:8083/toggl';
+
+  constructor(
+    private http: HttpClient,
+    protected localStorage: AsyncLocalStorage) {
+
+  }
+
+
+  getTogglTimers(): Observable<any> {
+    return Observable.create(observer => {
+      this.localStorage.getItem('togglToken').subscribe(token => {
+        console.log(token);
+        const headerDict = {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'togglToken': token
+        };
+
+        const requestOptions = {
+          headers: new HttpHeaders(headerDict),
+        };
+        this.http.get<TogglTimer[]>(this.togglUrl, requestOptions).subscribe(observer)
+      });
+    });
+  }
+
+}
