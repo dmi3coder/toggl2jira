72b88b87641f1452db7510a056cc53258c84dd25
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-31 11:54:12 +0300

    Consuming summary endpoint on ui raw

--- toggl2jira-web/src/main/web-app/ui/src/app/converter/converter.component.ts
+++ toggl2jira-web/src/main/web-app/ui/src/app/converter/converter.component.ts
@@ -1,9 +1,9 @@
-import {Component, Injectable, OnInit} from '@angular/core';
+import {Component, Injectable, Input, OnInit} from '@angular/core';
 import {TogglTimer} from "../toggl-timer";
 import {element} from "protractor";
 import {TogglService} from "../toggl.service";
 import {Inject} from "@angular/compiler/src/core";
-
+import * as _ from "lodash";
 @Component({
   selector: 'app-converter',
   templateUrl: './converter.component.html',
@@ -11,13 +11,16 @@ import {Inject} from "@angular/compiler/src/core";
 })
 @Injectable()
 export class ConverterComponent implements OnInit {
-  elements: object[];
+  elements: any[];
 
   constructor(private service: TogglService) {
     service.getSummary().subscribe(items => {
       this.elements = items;
-      console.log("working");
-      console.log(items)
+      this.elements.forEach(item => {
+        let time = _.sumBy(item.second, el => {return (<any>el).dur});
+        item.summary = Math.floor(time/1000/60/60)+' hours '
+          +Math.round(time%(1000*60*60)/1000/60)+' minutes' //interval js libraries suck a lot
+      });
     });
   }
 
