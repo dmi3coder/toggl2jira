ae79bf2789f5667ae99a602fc15d09080d37a0bd
Dmitry Chaban <dmitrychaban@yahoo.com>
2018-03-31 10:11:30 +0300

    Manually fetching header tokens Removed toggl configurations

--- toggl2jira-web/src/main/kotlin/io/github/dmitrychaban/toggl2jira/controller/SummaryController.kt
+++ toggl2jira-web/src/main/kotlin/io/github/dmitrychaban/toggl2jira/controller/SummaryController.kt
@@ -1,17 +1,16 @@
 package io.github.dmitrychaban.toggl2jira.controller
 
 import io.github.dmitrychaban.java_jira_sdk_light.DefaultJiraApi
-import io.github.dmitrychaban.java_jira_sdk_light.JiraApi
 import io.github.dmitrychaban.java_jira_sdk_light.model.Issue
 import io.github.dmitrychaban.java_jira_sdk_light.model.JiraProject
-import io.github.dmitrychaban.java_toggl_sdk_light.TogglApi
 import io.github.dmitrychaban.java_toggl_sdk_light.TogglApiBuilder
 import io.github.dmitrychaban.java_toggl_sdk_light.model.Timer
 import org.joda.time.DateTime
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
@@ -20,14 +19,16 @@ import reactor.core.publisher.Mono
 @RequestMapping("/summary")
 class SummaryController {
 
-    @Autowired
-    lateinit var jiraApi: JiraApi
-
-    @Autowired
-    lateinit var togglApi: TogglApi
-
     @GetMapping
-    fun getSummary(): Mono<List<Pair<Issue,List<Timer>>>> {
+    fun getSummary(): Mono<List<Pair<Issue, List<Timer>>>> {
+        //TODO: Replace manual token fetching with bean
+        val curRequest = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request
+        val jiraToken = curRequest.getHeader("jiraToken")
+        val jiraMail = curRequest.getHeader("jiraMail")
+        val jiraApi = DefaultJiraApi(jiraToken, jiraMail)
+        val togglToken = curRequest.getHeader("togglToken")
+        val togglApi = TogglApiBuilder.with(togglToken).build()
+
         return Mono.create {
 
             val projects: List<JiraProject> = jiraApi.getJiraProjects().flatMapMany { Flux.fromIterable(it) }.collectList().block()!!
